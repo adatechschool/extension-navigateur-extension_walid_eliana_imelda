1cc505bc2cf4c3ed66c9725ab5dabb463b44e1e8
Arteinsana7 <147487242+Arteinsana7@users.noreply.github.com>
2024-01-11 10:49:40 +0100

    change

--- popup.js
+++ popup.js
@@ -1,43 +1,11 @@
-
-let state = {
-    selectedFont: 'monospace',
-    selectedFontSize: '16px'
-  };
+document.addEventListener('DOMContentLoaded', function() {
+    var replaceButton = document.getElementById('replace-button');
+    var newFontFamilyInput = document.getElementById('new-font-family');
   
-  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action === 'applyChanges') {
-      state.selectedFont = request.font;
-      state.selectedFontSize = request.fontSize;
-      applyChanges();
-    }
+    replaceButton.addEventListener('click', function() {
+      var newFontFamily = newFontFamilyInput.value || 'monospace';
+      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { action: 'replaceFontFamily', newFontFamily: newFontFamily });
+      });
+    });
   });
-  
-  function applyChanges() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id },
-        function: changeFontAndSize,
-        args: [state.selectedFont, state.selectedFontSize]
-      });
-      chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id },
-        function: changeSpecificLetters,
-        args: [state.selectedFont, ['p', 'b', 'z', 's']]
-      });
-    });
-  }
-  
-  function changeFontAndSize(font, fontSize) {
-    document.body.style.fontFamily = font;
-    document.body.style.fontSize = fontSize;
-  }
-  
-  function changeSpecificLetters(font, letters) {
-    document.querySelectorAll('*').forEach(element => {
-      const textContent = element.textContent || element.innerText || '';
-      const modifiedText = textContent.split('').map(char => {
-        return letters.includes(char.toLowerCase()) ? `<span style="font-family: ${font}; font-size: ${state.selectedFontSize}">${char}</span>` : char;
-      }).join('');
-      element.innerHTML = modifiedText;
-    });
-  }
